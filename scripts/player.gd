863bd1aee20df896e2628966c8d895f8ded690ae
Simon Häsler <simon.haesler@gmx.ch>
2025-02-01 22:11:14 +0100

    Test around with slope physics

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -610.0
+var jump_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,6 +30,12 @@ func handle_movement(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+	elif Input.is_action_just_pressed("jump") && jump_count > 0 && jump_count < 2 && !is_on_floor():
+		velocity.y = JUMP_VELOCITY;
+		jump_count += 1
+	elif jump_count > 0 && is_on_floor():
+		jump_count = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
