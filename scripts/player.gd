b7abb8fcfce4d743f1d7fe5ab7646704ce1f6654
Simon Häsler <simon.haesler@gmx.ch>
2025-02-01 19:42:33 +0100

    Add message on player load

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const JUMP_VELOCITY = -500.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Ready")
 	initialize_animations()
 
 func _physics_process(delta: float) -> void:
